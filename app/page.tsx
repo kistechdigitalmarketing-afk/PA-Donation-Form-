3ea1c75583b858b505ab2b17b1324a87c0f120e2
DIPSHA <dipshadas4@gmail.com>
2025-12-19 20:51:36 +0530

    made the scroller invisible

--- app/page.tsx
+++ app/page.tsx
@@ -6,21 +6,22 @@ import SupportPoints from '@/components/SupportPoints';
 
 export default function Home() {
   return (
-    <main className="w-full">
+    <main className="h-screen w-full flex flex-col overflow-hidden">
+      {/* Fixed header inside iframe */}
       <Header />
-      <div className="flex flex-col">
+
+      {/* Scrollable content area */}
+      <div className="flex-1 overflow-y-auto">
         {/* Form Section */}
-        <div className="bg-white p-3 sm:p-4 md:p-6 flex items-center justify-center">
+        <div className="bg-white p-3 sm:p-4 md:p-6 flex justify-center">
           <div className="w-full max-w-4xl">
             <DonationForm />
           </div>
         </div>
-        
-        {/* Support Points Section - Below Form */}
-        <div className="bg-amber-900 p-4 sm:p-6 md:p-8 lg:p-12 flex items-center">
-          <div className="w-full">
-            <SupportPoints />
-          </div>
+
+        {/* Support Points Section */}
+        <div className="bg-amber-900 p-4 sm:p-6 md:p-8 lg:p-12">
+          <SupportPoints />
         </div>
       </div>
     </main>
